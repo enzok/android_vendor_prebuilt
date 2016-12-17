91c313f3c56ce36d70c75b3ad0592a8e1972433f
beanstown106 <nbenis106@gmail.com>
2016-12-17 03:11:42 -0500

    prebuilt: add the rest of the prebuilt wallpapers *remove live earth on arm devices

--- prebuilt.mk
+++ prebuilt.mk
@@ -16,8 +16,26 @@
 PRODUCT_PACKAGES += \
     NexusWallpapersStubPrebuilt \
     WallpaperPickerGooglePrebuilt \
-    NexusLauncherPrebuilt
+    NexusLauncherPrebuilt \
+    WallpapersUsTwo
 
 # Include package overlays
 PRODUCT_PACKAGE_OVERLAYS += \
     vendor/prebuilt/overlay/
+
+# wallpaper location prop
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.wallpapers_loc_request_suw=true
+
+# live wallpapers arm64 only
+ifneq ($(filter angler bullhead marlin sailfish,$(TARGET_PRODUCT)),)
+PRODUCT_PACKAGES += \
+    WallpapersBReel \
+    libgdx.so \
+    libgeswallpapers-jni.so \
+    libjpeg.so
+
+PRODUCT_COPY_FILES += \
+    vendor/prebuilt/lib64/libgdx.so:system/lib64/libgdx.so \
+    vendor/prebuilt/lib64/libgeswallpapers-jni.so:system/lib64/libgeswallpapers-jni.so
+endif
