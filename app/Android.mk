4659de61cb237f5a2dc15e08f4aeb97d7e28168d
beanstown106 <nbenis106@gmail.com>
2017-02-18 02:33:54 -0500

    build Google Calculator 7.2.1 overwrite Calculator and ExactCalculator

--- app/Android.mk
+++ app/Android.mk
@@ -15,6 +15,17 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := CalculatorGooglePrebuilt
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := CalculatorGooglePrebuilt/CalculatorGooglePrebuilt.apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := APPS
+LOCAL_OVERRIDES_PACKAGES := Calculator \
+                            ExactCalculator
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := NexusLauncherIcons
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := NexusLauncherIcons/NexusLauncherIcons.apk
